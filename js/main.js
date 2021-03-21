f60b7fceb94991e2cf22c34c3f0dded33f94cf28
pandanom <stas110922@gmail.com>
2021-03-21 23:07:37 +0200

    leaderboard

--- js/main.js
+++ js/main.js
@@ -77,3 +77,60 @@ function sendMsg(){
 	}
 }
 
+
+
+function updateLeaderboardView() {
+	let scores = [
+    {name: "Player 1", score: 300},
+    {name: "Player 2", score: 370},
+    {name: "Player 3", score: 500},
+    {name: "Player 4", score: 430},
+    {name: "Player 5", score: 340},
+	{name: "Player 6", score: 320},
+    {name: "Player 7", score: 385},
+    {name: "Player 8", score: 520},
+    {name: "Player 9", score: 450},
+    {name: "Player 10", score: 250},
+	{name: "Player 11", score: 305},
+    {name: "Player 12", score: 375},
+    {name: "Player 13", score: 505},
+    {name: "Player 14", score: 435},
+    {name: "Player 15", score: 345},
+	{name: "Player 16", score: 325},
+    {name: "Player 17", score: 395},
+    {name: "Player 18", score: 525},
+    {name: "Player 19", score: 455},
+    {name: "Player 20", score: 255},
+	];
+    let leaderboard = document.getElementById("leaderboard");
+    leaderboard.innerHTML = "";
+
+    scores.sort(function(a, b){ return b.score - a.score  });
+    let elements = []; // we'll need created elements to update colors later on
+    // create elements for each player
+    for(let i=0; i<scores.length; i++) {
+        let name = document.createElement("div");
+        let score = document.createElement("div");
+		let spacer = document.createElement("div");
+        name.classList.add("lbName");
+        score.classList.add("lbScore");
+		spacer.classList.add("lbSpacer");
+        name.innerText = scores[i].name;
+        score.innerText = scores[i].score;
+        let scoreRow = document.createElement("div");
+        scoreRow.classList.add("row");
+        scoreRow.appendChild(name);
+		scoreRow.appendChild(spacer);
+        scoreRow.appendChild(score);
+        leaderboard.appendChild(scoreRow);
+
+        elements.push(scoreRow);
+
+    }
+
+    let colors = ["gold", "silver", "#cd7f32"];
+    for(let i=0; i < 3; i++) {
+        elements[i].style.color = colors[i];
+    }
+}
+
