c7535ad8f17efb99551d6c2ba7403d72171831ae
pandanom <stas110922@gmail.com>
2021-03-31 12:25:05 +0300

    gamecs board

--- ChessApp/ChessApp/Models/Game.cs
+++ ChessApp/ChessApp/Models/Game.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 namespace ChessApp.Models
 {
     public class Game
-    {        
+    {
         public string id { get; set; }
         public string chat { get; set; }
         public User host { get; set; }
         public User guest { get; set; }
         public string hostCS { get; set; }
         public string guestCS { get; set; }
-
+        public Figure[,] board {get;set;}
     }
 }
